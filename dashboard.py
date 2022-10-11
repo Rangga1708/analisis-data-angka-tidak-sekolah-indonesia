9d990edddc39e3d81503ee5d2d4af4e43ed70b12
Rangga1708 <ranggajaya.c@gmail.com>
2022-10-11 13:23:05 +0700

    Add Image

--- dashboard.py
+++ dashboard.py
@@ -2,8 +2,12 @@ import streamlit as st
 import pandas as pd
 import plotly.express as px
 import plotly.graph_objects as go
+from PIL import Image
 
 st.title("Alokasi Dana untuk Pendidikan. Perlukah?")
+
+image = Image.open('Pendidikan.jpg')
+st.image(image)
 
 '''
 Masyarakat Indonesia saat ini dihebohkan dengan keputusan pemerintah untuk
